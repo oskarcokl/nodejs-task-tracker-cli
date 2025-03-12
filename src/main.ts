521b2fc53a15c8f2d929b02fb99b3ebf9575adb3
oskarcokl <oskar.cokl@gmail.com>
2025-03-12 21:09:16 +0100

    feat: Add checking if command is valid.

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,39 @@
 
 import { exit } from 'process';
 
+// GLOBALS
+const validCommands = ['add', 'update', 'delete', 'mark-in-progress', 'mark-done', 'list'];
 // TODO: Check how to build this as a CLI application.
 
-if (process.argv.length == 2) {
-    // no command was provided print help message
-    printHelpMessage();
-    // Exit with error?
-    exit(1);
+function main() {
+    if (process.argv.length == 2) {
+        // no command was provided print help message
+        printHelpMessage();
+        // Exit with error?
+        exit(1);
+    }
+
+    // list of commands to support
+    // add
+    // update
+    // delete
+    // mark-in-progress
+    // mark-done
+    // list
+    // if the command is not one of this throw error or display helpful information
+
+    const command = process.argv[2];
+
+    if (!validCommands.includes(command)) {
+        console.error('Use a valid command');
+        printHelpMessage();
+        exit(1);
+    }
+    //# sourceMappingURL=main.js.map
 }
-
-// list of commands to support
-// add
-// update
-// delete
-// mark-in-progress
-// mark-done
-// list
-// if the command is not one of this throw error or display helpful information
-
-const command = process.argv[2];
-
-//# sourceMappingURL=main.js.map
 
 function printHelpMessage() {
     console.log('You should provide the tool with a command');
 }
+
+main();
